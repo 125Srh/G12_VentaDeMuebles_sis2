370d26ed272f7f516d6c90c02c1b7ba3dba79c01
Admin <Admin@LAPTOP-SEVH0L6B>
2025-02-03 19:30:42 -0400

    Primer commit

---
+++ src/main/java/com/mycompany/g12_ventademuebles_sis2/G12_ventaDeMuebles_sis2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.g12_ventademuebles_sis2;
+
+/**
+ *
+ * @author Admin
+ */
+public class G12_ventaDeMuebles_sis2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
